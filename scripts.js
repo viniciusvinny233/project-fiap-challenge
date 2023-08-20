64fce1a4d27125c64f5a4ca3b23b19e95d23cdc7
Marcos Vinicius <viniciusvinny233@gmail.com>
2023-08-20 14:04:17 -0300

    Adicionando box de postar

--- scripts.js
+++ scripts.js
@@ -1,9 +1,72 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const likeButtons = document.querySelectorAll('.like-button');
+document.getElementById('fechar-box-id').addEventListener('click', function() {
 
-    likeButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            alert('Post curtido!');
-        });
-    });
+    var contentDiv = document.getElementById('blackout-page-id');
+    var contentDiv1 = document.getElementById('box-post-id');
+    
+    if (contentDiv.classList.contains('hidden')) {
+        contentDiv.classList.remove('hidden');
+    } else {
+        contentDiv.classList.add('hidden');
+    }
+
+    if (contentDiv1.classList.contains('hidden')) {
+        contentDiv1.classList.remove('hidden');
+    } else {
+        contentDiv1.classList.add('hidden');
+    }
+});
+
+document.getElementById('postar-id').addEventListener('click', function() {
+
+    var contentDiv = document.getElementById('blackout-page-id');
+    var contentDiv1 = document.getElementById('box-post-id');
+    
+    if (contentDiv.classList.contains('hidden')) {
+        contentDiv.classList.remove('hidden');
+    } else {
+        contentDiv.classList.add('hidden');
+    }
+
+    if (contentDiv1.classList.contains('hidden')) {
+        contentDiv1.classList.remove('hidden');
+    } else {
+        contentDiv1.classList.add('hidden');
+    }
+});
+
+document.getElementById('postar-show-id').addEventListener('click', function() {
+
+    var contentDiv = document.getElementById('blackout-page-id');
+    var contentDiv1 = document.getElementById('box-post-id');
+    
+    if (contentDiv.classList.contains('hidden')) {
+        contentDiv.classList.add('hidden');
+    } else {
+        contentDiv.classList.add('hidden');
+    }
+
+    if (contentDiv1.classList.contains('hidden')) {
+        contentDiv1.classList.add('hidden');
+    } else {
+        contentDiv1.classList.add('hidden');
+    }
+});
+
+document.getElementById('publish-button-id').addEventListener('click', function() {
+
+    var contentDiv = document.getElementById('blackout-page-id');
+    var contentDiv1 = document.getElementById('box-post-id');
+    
+    if (contentDiv.classList.contains('hidden')) {
+        contentDiv.classList.add('hidden');
+    } else {
+        contentDiv.classList.add('hidden');
+    }
+
+    if (contentDiv1.classList.contains('hidden')) {
+        contentDiv1.classList.add('hidden');
+    } else {
+        contentDiv1.classList.add('hidden');
+    }
+    
 });
