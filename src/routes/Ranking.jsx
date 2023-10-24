c764137b3f5467a2b2c8ceed7c0bab9cbb5ab3d0
Marcos Vinicius <viniciusvinny233@gmail.com>
2023-10-23 21:51:49 -0300

    Ajuste de Ranking responsivo

--- src/routes/Ranking.jsx
+++ src/routes/Ranking.jsx
@@ -105,7 +105,7 @@ const Ranking = () => {
                </ConfigButtons>
             )}{" "}
          </div>
-         <div className="center">
+         <div className="center ">
             <PageTop>
                {shouldRenderAdditionalItems && (
                   <>
